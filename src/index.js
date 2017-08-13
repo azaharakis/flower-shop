d2b0baf5ed4c205a3234c526b9f54374331b3cdf
Alexander Zaharakis <alex.zaharakis@rea-group.com>
2017-08-13 18:11:32 +1000

    Wrap the app using node's util
    As we aren't strict on the requirements for outputing
    using this tool seems like a good fit.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 /* @flow */
 
 import app from './app';
+const util = require('util');
 
 /*eslint-disable no-console */
 console.log(
-    app(`
-        10 R12
-        15 L09
-        13 T58
-    `)
+    util.inspect(
+        app(`
+            10 R12
+            15 L09
+            13 T58
+        `),
+        { depth: null }
+    )
 );
