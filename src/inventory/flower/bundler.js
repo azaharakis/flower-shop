6b0445707125d140d6d9a01287761dd1f30d06d0
Alexander Zaharakis <alex.zaharakis@rea-group.com>
2017-08-13 19:42:46 +1000

    Tidy up bundler logic

--- src/inventory/flower/bundler.js
+++ src/inventory/flower/bundler.js
@@ -2,46 +2,46 @@
 
 import type {Bundle, Bundles} from './item';
 
+const generateAGroupedBundle = (bundle, size) => total => {
+    const numberOfTimes = Math.floor(total / size); //Number of times of we can fit a bundle in the total
+    let groupedBundle;
+    if(numberOfTimes >= 1){
+        groupedBundle = [numberOfTimes, bundle];
+        total = total - size * numberOfTimes;
+    }else{
+        groupedBundle = [0, bundle];
+    }
+    return [ total, groupedBundle ];
+};
+
 const evaluateBundles = (bundles, total, startAtIndex) => {
     let remaining = total; // the diff left over after the previous bundle has been applied
 
-    return bundles.reduce((accum, bundle, i) => {
+    return bundles.reduce((accumulator, bundle, i) => {
         const [size] = bundle;
+        const calculateGroupedBundleFrom = generateAGroupedBundle(bundle, size);
+
         //If there is no remaining values or;
-        //Are we are starting from a higher index
-        if(remaining === 0 || startAtIndex >= i) {
-            accum.push([0, bundle]);
-            return accum;
+        //we are starting at the next highest index
+        if(remaining === 0 || startAtIndex > i) {
+            return [
+                ...accumulator,
+                [0, bundle]
+            ];
         }
 
-        //first time
-        if(accum.length === 0){
-            const numberOfTimes = Math.floor(total / size); //Number of times of we can fit a bundle in the total
-            if(numberOfTimes >= 1){
-                accum.push([numberOfTimes, bundle]);
-                remaining = total - size * numberOfTimes;
-            }else{
-                accum.push([0, bundle]);
-                remaining = total;
-            }
-            return accum;
-        }
+        const [newTotal, groupedBundle] = calculateGroupedBundleFrom(remaining);
+        remaining = newTotal;
 
-        //all others
-        const numberOfTimes = Math.floor(remaining / size);
-        if(numberOfTimes >= 1){
-            accum.push([numberOfTimes, bundle]);
-            remaining = remaining - size * numberOfTimes;
-        } else {
-            accum.push([0, bundle]);
-        }
-
-        //If there's a remainder on the final entry discard;
+        //If there's a remainder on the final entry discard our results;
         if(i === bundles.length - 1 && remaining > 0){
             return [];
         }
 
-        return accum;
+        return [
+            ...accumulator,
+            groupedBundle
+        ];
     }, []);
 };
 
@@ -53,7 +53,9 @@ export default (bundles: Bundles, total: Quantity): Array<[Quantity, Bundle]> =>
 
     let optimizedBundles = [];
     for(let i = 0; i < sortedBundles.length -1; i++){
-        optimizedBundles = evaluateBundles(sortedBundles, total, i-1);
+        optimizedBundles = evaluateBundles(sortedBundles, total, i);
+        //If there are results we have a group of optimized bundles, otherwise
+        //we are going to try again starting from the next largest bundle
         if(optimizedBundles.length > 0) break;
     }
 
